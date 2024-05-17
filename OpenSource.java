fd4707360fb706814131232e3295bc23515ac9c6
24amaris <147129858+24amaris@users.noreply.github.com>
2024-05-17 23:44:55 +0300

    Exercițiu 4

---
+++ OpenSource.java
@@ -0,0 +1,19 @@
+
+public class OpenSource extends Project{
+
+	private final int MAX_MEMBERS=Integer.MAX_VALUE;
+	private String mailing_list;
+	public OpenSource(String title, String objective, Member manager, String mailing_list, long funds) {
+		super(title, objective, manager, funds);
+		this.mailing_list=mailing_list;
+	}
+
+	public int getMAX_MEMBERS() {
+		return this.MAX_MEMBERS;
+	}
+
+	public double getRisk() {
+		return this.getMembers().size()/this.getFunds();
+	}
+
+}
